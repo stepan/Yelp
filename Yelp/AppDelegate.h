e0bce2574872c71e2a9b1ad4b152ea7af8ab9231
Stepan Grigoryan <stepang@gmail.com>
2014-03-20 13:13:42 -0700

    Initial Commit

---
+++ Yelp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Yelp
+//
+//  Created by Stepan Grigoryan on 3/20/14.
+//  Copyright (c) 2014 Stepan Grigoryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Yelp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Yelp
+//
+//  Created by Stepan Grigoryan on 3/20/14.
+//  Copyright (c) 2014 Stepan Grigoryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
